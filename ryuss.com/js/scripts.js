e505e092ab5bfd6b249e8d35f5a1b4325cb6d0b4
Seungwoo Yoo <ryuryu10@naver.com>
2022-04-01 16:38:33 +0900

    z

--- ryuss.com/js/scripts.js
+++
@@ -1,9 +0,0 @@
-var imgArray=new Array();
-imgArray[0]="../assets/img/astronomy-1868560.jpg";
-imgArray[1]="../assets/img/bg-masthead.jpg";
-function showImage(){
-    var imgNum=Math.round(Math.random()*3);
-    var objImg=document.getElementById("masthead");
-    objImg.src=imgArray[imgNum];
-    setTimeout(showImage,2000); 
-}
