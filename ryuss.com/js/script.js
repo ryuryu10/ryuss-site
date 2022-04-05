41d39188913b74fee02d4f3968f7e6b46a884895
Seungwoo Yoo <ryuryu10@naver.com>
2022-04-06 00:42:20 +0900

    coommit

--- ryuss.com/js/script.js
+++ ryuss.com/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor: ['#FFFFFF', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+		sectionsColor: ['#FFFFFF', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff', '#ccddff']
 	});
 });
